1d2c89d2cc839002ae3e575cb64aa391c31a4298
1mazin <78849132+1mazin@users.noreply.github.com>
2022-07-04 22:59:24 +0530

    Create Intersection of two linked list.java

---
+++ LinkedList/Intersection of two linked list.java
@@ -0,0 +1,17 @@
+/*Given the heads of two singly linked-lists headA and headB, return the node at which the two lists intersect. 
+  If the two linked lists have no intersection at all, return null.*/
+
+public class Solution {
+    public ListNode getIntersectionNode(ListNode headA, ListNode headB) {
+        
+         ListNode a=headA;
+         ListNode b=headB;
+         while(a!=b)
+         {
+             a=a==null?headB:a.next;
+             b=b==null?headA:b.next;
+         }
+        return b;
+        
+    }
+}
